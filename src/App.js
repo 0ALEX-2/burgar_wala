e80f4d9aed4cc7eaff512da9d4d330c27cc4e40d
0ALEX-2 <sakilahmed965@gmail.com>
2023-10-07 22:54:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-
+import "./styles/app.scss";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <h1>App</h1>
     </div>
   );
 }
--- src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div className="text-2xl text-black h-[100vh] bg-slate-400">Home</div>;
-};
-
-export default Home;
