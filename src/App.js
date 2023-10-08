f610edba597e9c429523f18b65df95e8ff31a8e5
0ALEX-2 <sakilahmed965@gmail.com>
2023-10-08 11:48:01 +0530

    home animate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/home/layout/Header";
 import { Routes, Route } from "react-router-dom";
 import "./styles/app.scss";
 import "./styles/header.scss";
+import "./styles/home.scss";
 import Home from "./components/home/Home";
 
 function App() {
