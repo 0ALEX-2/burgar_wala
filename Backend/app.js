874a88e1df60c6f8565fc5ca3ca882977f9b38f3
0ALEX-2 <sakilahmed965@gmail.com>
2023-10-24 12:29:57 +0530

    updated auth

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import passport from "passport";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 export default app;
@@ -8,8 +13,24 @@ dotenv.config({
   path: "./config/config.env",
 });
 
+//Midlewares
+app.use(cookieParser);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
 connectPassport();
 
-//importing routes
-import userRoute from "./routes/user.js";
 app.use("/api/v1", userRoute);
+
+//Error middleware
+app.use(errorMiddleware);
+
+
---
+++ Backend/middlewares/auth.js
@@ -0,0 +1,10 @@
+import ErrorHandler from "../utils/ErrorHandler.js";
+
+export const isAuthenticated = (req, res, next) => {
+  const token = req.cookies["connect.sid"];
+
+  if (!token) {
+    return next(new ErrorHandler("Not logged in", 401));
+  }
+  next();
+};
