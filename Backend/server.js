3b862a112aaa595de9b02eb69fe32a4c3a9c59a7
0ALEX-2 <sakilahmed965@gmail.com>
2023-10-27 07:19:24 +0530

    razorpay setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,13 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
+import Razorpay from "razorpay";
 
 connectDB();
+
+export const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: RAZORPAY_API_SECRET,
+});
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Working</h1>");
